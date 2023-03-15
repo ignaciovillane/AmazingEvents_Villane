344dc9b8244b7e5a25e214163115da3909cce89e
ignacio villane <ignaciovillane@gmail.com>
2023-03-15 03:56:29 -0300

    task 3 finalizada

---
+++ scripts/details.js
@@ -0,0 +1,88 @@
+const containerCards = document.getElementById("containerCards") //Selecciono un elemento HTML del DOM utilizando document.getElementById() y lo almaceno en una variable llamada containerCards
+const querySearch = document.location.search //obtengo la cadena de consulta de la URL actual de la página y la almaceno en una variable llamada querySearch. La cadena de consulta es todo lo que aparece después del signo "?" en la URL.
+const id = new URLSearchParams(querySearch).get("id") //Obtengo el valor del parámetro "id" de la URL utilizando URLSearchParams() y lo almaceno en una variable llamada id
+
+const contenedor = document.getElementById('contenedor')
+const contenedorCheck = document.getElementById('checkContainer')
+const input = document.querySelector('input')
+
+function getEvents(array) {
+    const eventNew = array.map(e => {
+        let aux = {}
+        aux.image = e.image
+        aux.name = e.name
+        aux.date = e.date
+        aux.description = e.description
+        aux.category = e.category
+        aux.place = e.place
+        aux.capacity = e.capacity
+        aux.assistance = e.assistance
+        aux.price = e.price
+        aux._id = e._id
+        return aux
+    })
+    return eventNew
+}
+
+function findId(array, ident) {
+    let arrayID = array.find(eventDetail => eventDetail._id == ident)
+    return arrayID
+}
+
+// function showCard(array) {
+//     containerCards.innerHTML = `
+
+// <div class="card" style="width: 18rem;">
+//     <div class="body-details">
+//         <div class="card-horizontal">
+//             <div >
+//                 <img  src="${array.image}" class="card-img-top" alt="Card image">
+//             </div>
+
+//             <div class="card-body">
+// <h4 class="card-title">Title: ${array.name}</h4>
+// <p class="card-text">Date: ${array.date}</p>
+// <p class="card-text">Description: ${array.description}</p>
+// <p class="card-text">Category: ${array.category}</p>
+// <p class="card-text">Place: ${array.place}</p>
+// <p class="card-text">Capacity: ${array.capacity}</p>
+// <p class="card-text">Assistance: ${array.assistance}</p>
+// <p class="card-text">Price: ${array.price}</p>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+
+//    `
+// }
+function showCard(array) {
+    containerCards.innerHTML = `
+
+    <div class="card mb-3" style="max-width: 100%;">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img class="img-fluid" src="${array.image}">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+        <h4 class="card-title">Title: ${array.name}</h4>
+        <p class="card-text">Date: ${array.date}</p>
+        <p class="card-text">Description: ${array.description}</p>
+        <p class="card-text">Category: ${array.category}</p>
+        <p class="card-text">Place: ${array.place}</p>
+        <p class="card-text">Capacity: ${array.capacity}</p>
+        <p class="card-text">Assistance: ${array.assistance}</p>
+        <p class="card-text">Price: ${array.price}</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+   `
+}
+
+let arr = getEvents(data.events)
+console.log(arr)
+let arr2 = findId(arr, id)
+console.log(arr2)
+showCard(arr2)
--- scripts/past_events.js
+++ scripts/past_events.js
@@ -1,25 +1,103 @@
-const divElementos = document.getElementById('past_events_cards')
+const contenedor = document.getElementById('contenedor')
+const contenedorCheck = document.getElementById('checkContainer')
+const input = document.querySelector('input')
 
-let tarjetas = ''
+input.addEventListener('input', superFilter)
 
-for (e of data.events) {
-    if (Date.parse(`${e.date}`) < Date.parse(data.currentDate)) {
-        tarjetas +=
-            `<div class="d-flex flex-wrap gap-3 justify-content-center p-3 bg-secondary rounded-3">
-        <div class="card" style="width: 18rem;">
-        <img src="${e.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-       <h5 class="card-title">${e.name}</h5>
-       <p class="card-text">${e.description}</p>
-       </div>
-       <div class="d-flex justify-content-between">
-       <h6>Price $ ${e.price}<h6>
-       <a href="./details.html" class="btn btn-primary">See more...</a>
-       </div>
-       </div>
-       </div>`
+contenedorCheck.addEventListener('change', superFilter)
+
+
+function superFilter() {
+    let firstFilter = filterText(showDetail(data.events), input.value)
+    let secondFilter = filterCategory(firstFilter)
+    showCard(secondFilter)
+}
+
+
+function showCheck(array) {
+    let arrayCategory = array.map(e => e.category)
+
+    let setCategory = new Set(arrayCategory)
+
+    let arrayChecks = Array.from(setCategory)
+
+    let checkboxes = ''
+    arrayChecks.forEach(element => {
+        checkboxes += `<div class="form-check ">
+                <input class="form-check-input" type="checkbox"  id="${element}" value="${element}">
+                <label class="form-check-label" for="${element}">${element}</label>
+              </div>`
+
+        contenedorCheck.innerHTML = checkboxes
+    })
+}
+
+function showCard(array) {
+    if (array.length == 0) {
+        contenedor.innerHTML = `<h2 class="display-1 fw-bolder">No existe el evento</h2>`
+        return
+    }
+    let tarjetas = ''
+    for (e of array) {
+        if (Date.parse(`${e.date}`) < Date.parse(data.currentDate)) {
+            tarjetas +=
+                `
+                        <div class="card" style="width: 18rem;">
+                        <img src="${e.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${e.name}</h5>
+                            <p class="card-text">${e.description}</p>
+                        </div>
+                       <div class="d-flex justify-content-between">
+                       <h6>Price $ ${e.price}<h6>
+                       <a href="./details.html?id=${e.id}" class="btn btn-primary">See more...</a>
+                       </div>
+                       </div>
+                       `
+        }
+        console.log(tarjetas);
+        contenedor.innerHTML = tarjetas
     }
 }
-console.log(tarjetas);
 
-divElementos.innerHTML = tarjetas
+function filterText(array, text) {
+    let arrayFiltred = array.filter(element => element.name.toLowerCase().includes(text.toLowerCase()))
+    return arrayFiltred
+}
+
+function filterCategory(array) {
+    let checkboxes = document.querySelectorAll("input[type='checkbox']")
+    console.log(checkboxes);
+    let arrayChecks = Array.from(checkboxes)
+    let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+    console.log(arrayChecksChecked);
+    let arrayChecksCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+    console.log(arrayChecksCheckedValues);
+    let arrayFiltred = array.filter(element => arrayChecksCheckedValues.includes(element.category))
+    console.log(arrayFiltred);
+    if (arrayChecksChecked.length > 0) {
+        return arrayFiltred
+    }
+    return array
+}
+showCard(showDetail(data.events))
+showCheck(data.events)
+
+function showDetail(array) {
+    const eventDetail = array.map(e => {
+        let aux = {}
+        aux.image = e.image
+        aux.name = e.name
+        aux.date = e.date
+        aux.description = e.description
+        aux.category = e.category
+        aux.place = e.place
+        aux.capacity = e.capacity
+        aux.assistance = e.assistance
+        aux.price = e.price
+        aux.id = e._id
+        return aux
+    })
+    return eventDetail
+
+}
